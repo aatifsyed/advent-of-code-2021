d7f0895dc7350b2d4da3bcbc31690cf13fb297dc
Aatif Syed <aatif@aatifsyed.uk>
2021-12-07 02:03:13 +0000

    tweaks

--- src/lanternfish.rs
+++ src/lanternfish.rs
@@ -8,14 +8,14 @@ extern crate test;
 const INPUT: &str = include_str!("./inputs/2021/6.txt");
 
 fn do_part1(input: &str) -> anyhow::Result<usize> {
-    let mut fish = LanternFish::try_from_iter(parse(input)?).unwrap();
+    let mut fish = LanternFish::from_csv(input)?;
     for _ in 0..80 {
         fish = fish.step_simulation()
     }
     Ok(fish.total())
 }
 fn do_part2(input: &str) -> anyhow::Result<usize> {
-    let mut fish = LanternFish::try_from_iter(parse(input)?).unwrap();
+    let mut fish = LanternFish::from_csv(input)?;
     for _ in 0..256 {
         fish = fish.step_simulation()
     }
@@ -27,15 +27,6 @@ benchtest::benchtest! {
     part2: do_part2(test::black_box(INPUT)).unwrap() => 1746710169834
 }
 
-fn parse(input: &str) -> anyhow::Result<Vec<usize>> {
-    input
-        .trim()
-        .split(',')
-        .map(str::parse::<usize>)
-        .collect::<Result<_, _>>()
-        .context("Invalid input format")
-}
-
 #[derive(Debug, Default, Clone, Copy)]
 struct LanternFish {
     timers: [usize; 9],
@@ -45,13 +36,17 @@ impl LanternFish {
     fn total(&self) -> usize {
         self.timers.into_iter().sum()
     }
-    // Can't blanket impl
-    fn try_from_iter(iter: impl IntoIterator<Item = usize>) -> anyhow::Result<Self> {
-        iter.into_iter()
-            .try_fold(LanternFish::default(), |mut acc, el| {
-                ensure!(el < 9, "Invalid fish lifetime: {}", el);
+    fn from_csv(csv: &str) -> anyhow::Result<Self> {
+        csv.trim()
+            .split(',')
+            .map(str::parse::<usize>)
+            .collect::<Result<Vec<_>, _>>()
+            .context("Must be a single line of CSVs")?
+            .into_iter()
+            .try_fold(Self::default(), |mut acc, el| {
+                ensure!(el < 9, "Fish must be =< 8, not {}", el);
                 acc.timers[el] += 1;
-                anyhow::Ok(acc)
+                Ok(acc)
             })
     }
     fn step_simulation(mut self) -> Self {
